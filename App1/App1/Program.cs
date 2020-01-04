98a8952a47b5e284a08fc1a1b3004da8b735983b
Ugnius <ugniusnaujokas@gmail.com>
2020-01-04 12:04:23 +0200

    Completed App1

---
+++ App1/App1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> skaiciai = new List<int>();
+            var skaicius = new Random();
+
+            
+            for(int i=0; i<10; i++)
+            {
+                skaiciai.Add(skaicius.Next(2, 10000));
+            }
+            foreach(int number in skaiciai)
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine(number);
+            }
+            Console.Read();
+        }
+
+    }
+}
