b4e6a034c4a243583f0f786d3b1d20fba0c599a8
Ugnius <ugniusnaujokas@gmail.com>
2020-01-04 12:26:41 +0200

    Start of App4

---
+++ App4/App4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> pazymiai = new List<int>();
+            int pasirinkimas;
+            for(; ; )
+            {
+                Console.WriteLine("Iveskite pasirinkima: 0 - papildyti, 1 - istrinti, 2 - iterpti, 3 - parodyti pazymiu vidurki, 4 - iseiti is programos");
+                String ivestasSkaicius = Console.ReadLine();
+                pasirinkimas = Convert.ToInt32(ivestasSkaicius);
+                switch(pasirinkimas)
+            }
+        }
+    }
+}
